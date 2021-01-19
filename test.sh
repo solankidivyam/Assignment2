6aaef75561c2b02b4c74d44c91d662f236be69e6
solankidivyam <53675889+solankidivyam@users.noreply.github.com>
2021-01-19 11:48:15 +0530

    Assignment2

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo This is the first program
+
+echo Enter any three arguments
+read arg1 arg2 arg3
+
+echo The argument one is $arg1
+echo The argument two is $arg3
+echo The argument three is $arg3
